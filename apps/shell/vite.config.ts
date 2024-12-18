24a577a370e1ecf92e3a69821365376b6ba98790
samuel.documet <samuel.documet@pagonxt.com>
2024-12-18 21:14:19 +0100

    core(XXXX-0000): add vitest config for any project in apps

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -1,10 +1,22 @@
-/// <reference types='vitest' />
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    coverage: {
+      enabled: true,
+      reporter: ['json', 'lcov', 'text', 'clover', 'html'],
+      reportsDirectory: '../../coverage/apps/shell',
+      provider: 'v8',
+    },
+  },
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/shell',
   server: {
