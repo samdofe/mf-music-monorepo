afbdbc0b2561d7c8c46fb6504a8cf80228747984
samuel.documet <samuel.documet@pagonxt.com>
2024-12-19 19:10:40 +0100

    feat(XXXX-0003): header implementation ok

--- libs/cdk-header/src/lib/cdk-header.tsx
+++ libs/cdk-header/src/lib/cdk-header.tsx
@@ -3,8 +3,9 @@ import { ReactElement } from 'react';
 
 export function CdkHeader({title}: {title: string}): ReactElement {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to { title }</h1>
+    <div className={styles['header']}>
+      <span className={styles['header__title']}>{title}</span>
+      <span className={styles['header__icon']}>icono</span>
     </div>
   );
 }
