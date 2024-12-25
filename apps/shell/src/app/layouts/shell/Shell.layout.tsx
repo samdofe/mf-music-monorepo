84369805ce2ed0b3e0b0117e303b963c6761f1bb
samuel.documet <samuel.documet@pagonxt.com>
2024-12-25 21:53:48 +0100

    feat(XXXX-0006): cdk thumbnail common component implementation ok

---
+++ apps/shell/src/app/layouts/shell/Shell.layout.tsx
@@ -0,0 +1,18 @@
+import { Outlet, useNavigation } from 'react-router-dom';
+import React from 'react';
+import { CdkHeader } from '@inditex/cdk-header';
+//import { ThreeDotsScaleIcon } from '@inditex/icons';
+import styles from './shell.layout.module.scss';
+
+export const ShellLayoutComponent = () => {
+  const navigation = useNavigation();
+
+  return (
+    <div className={styles['shell']}>
+      <div className={styles['shell__layout']}>
+        <CdkHeader title="Podcaster" showIcon={navigation.state !== 'idle'} />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
