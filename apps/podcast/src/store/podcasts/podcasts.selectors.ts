9fc70a6b4c37e4c04f9fa5f7c60fc8ead53ebd2c
samuel.documet <samuel.documet@pagonxt.com>
2025-01-02 14:59:33 +0100

    feat(XXXX-0009): integrate episodes list in podcast details

---
+++ apps/podcast/src/store/podcasts/podcasts.selectors.ts
@@ -0,0 +1,57 @@
+import { useContext } from 'react';
+import { IEpisodesList, IPodcast } from '@models';
+import { PodcastContext } from './podcasts.store';
+
+export const usePodcastStore=()=>{
+  const context = useContext(PodcastContext);
+  if(!context){
+    throw new Error(' usePodcastStore must be used within a PodcastStoreProvider');
+  }
+
+  return context;
+}
+
+export const useGetPodcastsSelector = ()=> {
+  const [podcastStore] = usePodcastStore();
+  return podcastStore.podcasts;
+}
+
+export const useGetSelectedPodcastSelector = ()=> {
+  const [podcastStore] = usePodcastStore();
+  return podcastStore.selectedPodcast;
+}
+
+export const useGetEpisodesListSelector = ()=> {
+  const [podcastStore] = usePodcastStore();
+  return podcastStore.episodesList;
+}
+
+export const useSetPodcastsSelector = ()=>{
+  const [ podcastsStore , setPodcastStore] = usePodcastStore();
+  return (podcasts: IPodcast[]) =>
+  setPodcastStore({
+    ...podcastsStore,
+    podcasts
+  });
+}
+
+export const useSetSelectedPodcastSelector = ()=>{
+  const [ podcastStore , setPodcastStore] = usePodcastStore();
+
+  return (selectedPodcast: IPodcast | undefined)=> setPodcastStore({
+    ...podcastStore,
+    selectedPodcast: selectedPodcast
+  });
+
+}
+
+export const useSetEpisodesListSelector = ()=>{
+  const [ podcastStore , setPodcastStore] = usePodcastStore();
+  return (episodesList: IEpisodesList | undefined)=>
+  setPodcastStore({
+    ...podcastStore,
+    episodesList: episodesList
+  });
+}
+
+
