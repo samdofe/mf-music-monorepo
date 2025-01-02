8da4f33ffdf0140ff5749eb05f4db25fe3ea471b
samuel.documet <samuel.documet@pagonxt.com>
2025-01-02 11:13:35 +0100

    feat(XXXX-0008): add cdk card to podcast-details implemented ok

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './routing/route-config';
+import './app.module.scss';
 
 const router = createBrowserRouter(routes);
 
