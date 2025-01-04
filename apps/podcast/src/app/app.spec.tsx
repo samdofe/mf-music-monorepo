d4ab6e89eadca0b2bf6feec55f3a53e07abb3627
samuel.documet <samuel.documet@pagonxt.com>
2025-01-04 18:13:36 +0100

    test(XXXX-0013): App test ok

--- apps/podcast/src/app/app.spec.tsx
+++ apps/podcast/src/app/app.spec.tsx
@@ -1,15 +1,35 @@
-import { render } from '@testing-library/react';
-
+import { render, screen } from '@testing-library/react';
 import App from './app';
+import { RouterTestProviders } from '@inditex/router';
 
-describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
-  });
+const mocks = vi.hoisted(() => {
+  return {
+    usePodcastsLoad: vi.fn(),
+  };
+});
 
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/PODCAST Remote app/gi)).toBeTruthy();
+vi.mock("@api", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("@api")>();
+  return {
+    ...actual,
+    usePodcastsLoad: mocks.usePodcastsLoad,
+  };
+});
+
+const renderComponent = () =>
+  render(
+    <RouterTestProviders>
+      <App />
+    </RouterTestProviders>
+  );
+
+describe('App Component', () => {
+  it('should render PodcastStoreProvider, Routes and mocked route content', () => {
+    // Render the App component inside BrowserRouter (needed for routing context)
+    mocks.usePodcastsLoad.mockReturnValue([]);
+    renderComponent();
+
+    // Assert that PodcastStoreProvider is present (by checking for something inside it)
+    expect(screen.getByText('NO RESULTS FOUND')).toBeInTheDocument();
   });
 });
