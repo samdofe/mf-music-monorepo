9893afe6fd5fb8bfff69086b872b5d97e96d4561
samuel.documet <samuel.documet@pagonxt.com>
2025-01-04 02:54:26 +0100

    refactor(XXXX-0011): move cdk-thumbnail into cdk-libs

--- libs/cdk-libs/cdk-thumbnail/vite.config.ts
+++ libs/cdk-libs/cdk-thumbnail/vite.config.ts
@@ -9,7 +9,7 @@ import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/libs/cdk-thumbnail',
+  cacheDir: '../../../node_modules/.vite/libs/cdk-thumbnail',
   plugins: [
     react(),
     nxViteTsPaths(),
@@ -23,7 +23,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../../dist/libs/cdk-thumbnail',
+    outDir: '../../../dist/libs/cdk-libs/cdk-thumbnail',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
@@ -60,7 +60,7 @@ export default defineConfig({
         '**/index.ts',
         '**/*.d.ts',
       ],
-      reportsDirectory: '../../coverage/libs/cdk-thumbnail',
+      reportsDirectory: '../../../coverage/cdk-libs/libs/cdk-thumbnail',
       provider: 'v8'
     }
   }
